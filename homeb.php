75905b17165c95b0a8ef464343af1efbe2f69d77
Jhonathan <jacireno@hotmail.com>
2019-02-24 17:03:04 -0500

    Vista de los once elementos en homeb.php con animación al clicken el titulo del primero de ellos

--- homeb.php
+++ homeb.php
@@ -18,10 +18,10 @@
     <section class="container-body ">
         <div class="content-body">
 
-            <div class="card-sala">
+            <div id="card-sala1" class="card-sala "> <!--active--> 
                 <div class="card-content">
-                    <div class="card-title">B201</div>
-                    <div class="card-body">
+                    <div id="slide-card" class="card-title ">B201</div> <!--active-->
+                    <div id="card-body1" class="card-body "> <!--active-->
                         <div class="card-element">Capacidad: 00</div>
                         <div class="card-element">Equipos de Computo: 00</div>
                         <div class="card-element">Aire Acondicionado: Si</div>
@@ -33,10 +33,10 @@
                     </div>
                 </div>
             </div>
-            <div class="card-sala">
+            <div id="card-sala2" class="card-sala "> <!--active--> 
                 <div class="card-content">
-                    <div class="card-title">B201</div>
-                    <div class="card-body">
+                    <div id="slide-card2" class="card-title ">B201</div> <!--active-->
+                    <div id="card-body2" class="card-body "> <!--active-->
                         <div class="card-element">Capacidad: 00</div>
                         <div class="card-element">Equipos de Computo: 00</div>
                         <div class="card-element">Aire Acondicionado: Si</div>
@@ -48,10 +48,10 @@
                     </div>
                 </div>
             </div>
-            <div class="card-sala">
+            <div id="card-sala3" class="card-sala "> <!--active--> 
                 <div class="card-content">
-                    <div class="card-title">B201</div>
-                    <div class="card-body">
+                    <div id="slide-card3" class="card-title ">B201</div> <!--active-->
+                    <div id="card-body3" class="card-body "> <!--active-->
                         <div class="card-element">Capacidad: 00</div>
                         <div class="card-element">Equipos de Computo: 00</div>
                         <div class="card-element">Aire Acondicionado: Si</div>
@@ -63,10 +63,10 @@
                     </div>
                 </div>
             </div>
-            <div class="card-sala">
+            <div id="card-sala4" class="card-sala "> <!--active--> 
                 <div class="card-content">
-                    <div class="card-title">B201</div>
-                    <div class="card-body">
+                    <div id="slide-card4" class="card-title ">B201</div> <!--active-->
+                    <div id="card-body4" class="card-body "> <!--active-->
                         <div class="card-element">Capacidad: 00</div>
                         <div class="card-element">Equipos de Computo: 00</div>
                         <div class="card-element">Aire Acondicionado: Si</div>
@@ -78,10 +78,11 @@
                     </div>
                 </div>
             </div>
-            <div class="card-sala">
+            
+            <div id="card-sala5" class="card-sala "> <!--active--> 
                 <div class="card-content">
-                    <div class="card-title">B201</div>
-                    <div class="card-body">
+                    <div id="slide-card5" class="card-title ">B201</div> <!--active-->
+                    <div id="card-body5" class="card-body "> <!--active-->
                         <div class="card-element">Capacidad: 00</div>
                         <div class="card-element">Equipos de Computo: 00</div>
                         <div class="card-element">Aire Acondicionado: Si</div>
@@ -93,10 +94,10 @@
                     </div>
                 </div>
             </div>
-            <div class="card-sala">
+            <div id="card-sala6" class="card-sala "> <!--active--> 
                 <div class="card-content">
-                    <div class="card-title">B201</div>
-                    <div class="card-body">
+                    <div id="slide-card6" class="card-title ">B201</div> <!--active-->
+                    <div id="card-body6" class="card-body "> <!--active-->
                         <div class="card-element">Capacidad: 00</div>
                         <div class="card-element">Equipos de Computo: 00</div>
                         <div class="card-element">Aire Acondicionado: Si</div>
@@ -108,10 +109,10 @@
                     </div>
                 </div>
             </div>
-            <div class="card-sala">
+            <div id="card-sala7" class="card-sala "> <!--active--> 
                 <div class="card-content">
-                    <div class="card-title">B201</div>
-                    <div class="card-body">
+                    <div id="slide-card7" class="card-title ">B201</div> <!--active-->
+                    <div id="card-body7" class="card-body "> <!--active-->
                         <div class="card-element">Capacidad: 00</div>
                         <div class="card-element">Equipos de Computo: 00</div>
                         <div class="card-element">Aire Acondicionado: Si</div>
@@ -123,74 +124,79 @@
                     </div>
                 </div>
             </div>
-            <div class="card-sala">
+            <div id="card-sala8" class="card-sala "> <!--active--> 
+                <div class="card-content">
+                    <div id="slide-card8" class="card-title ">B201</div> <!--active-->
+                    <div id="card-body8" class="card-body "> <!--active-->
+                        <div class="card-element">Capacidad: 00</div>
+                        <div class="card-element">Equipos de Computo: 00</div>
+                        <div class="card-element">Aire Acondicionado: Si</div>
+                        <div class="card-element">Conexi&oacute;n TV: HDMI: 00 VGA: 00</div>
+                        <div class="card-element">Tablero: Acr&iacute;lico, Tiza</div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="card-element enlace"><a href="#">VER HORARIOS</a></div>
+                    </div>
+                </div>
+            </div>
+            <div id="card-sala9" class="card-sala "> <!--active--> 
+                <div class="card-content">
+                    <div id="slide-card9" class="card-title ">B201</div> <!--active-->
+                    <div id="card-body9" class="card-body "> <!--active-->
+                        <div class="card-element">Capacidad: 00</div>
+                        <div class="card-element">Equipos de Computo: 00</div>
+                        <div class="card-element">Aire Acondicionado: Si</div>
+                        <div class="card-element">Conexi&oacute;n TV: HDMI: 00 VGA: 00</div>
+                        <div class="card-element">Tablero: Acr&iacute;lico, Tiza</div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="card-element enlace"><a href="#">VER HORARIOS</a></div>
+                    </div>
+                </div>
+            </div>
+            <div id="card-sala10" class="card-sala "> <!--active--> 
+                <div class="card-content">
+                    <div id="slide-card10" class="card-title ">B201</div> <!--active-->
+                    <div id="card-body10" class="card-body "> <!--active-->
+                        <div class="card-element">Capacidad: 00</div>
+                        <div class="card-element">Equipos de Computo: 00</div>
+                        <div class="card-element">Aire Acondicionado: Si</div>
+                        <div class="card-element">Conexi&oacute;n TV: HDMI: 00 VGA: 00</div>
+                        <div class="card-element">Tablero: Acr&iacute;lico, Tiza</div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="card-element enlace"><a href="#">VER HORARIOS</a></div>
+                    </div>
+                </div>
+            </div>
+            <div id="card-sala11" class="card-sala "> <!--active--> 
+                <div class="card-content">
+                    <div id="slide-card11" class="card-title ">B201</div> <!--active-->
+                    <div id="card-body11" class="card-body "> <!--active-->
+                        <div class="card-element">Capacidad: 00</div>
+                        <div class="card-element">Equipos de Computo: 00</div>
+                        <div class="card-element">Aire Acondicionado: Si</div>
+                        <div class="card-element">Conexi&oacute;n TV: HDMI: 00 VGA: 00</div>
+                        <div class="card-element">Tablero: Acr&iacute;lico, Tiza</div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="card-element enlace"><a href="#">VER HORARIOS</a></div>
+                    </div>
+                </div>
+            </div>
+            
+            
+           <!-- <div class="card-sala">
                 <div class="card-content">
                     <div class="card-title txt-center">Importante!</div>
                     <div class="card-body txt-justify">
                         <div class="card-element">Verifica si las salas están disponibles, en caso de que lo esté, se puede solicitar. Para cualquier información realizarla via email haciendo clic en: <a href="mailto:monitor.ibague@uan.edu.co">monitor.ibague@uan.edu.co</a>, o directamente con el monitor de turno</div>
                     </div>
                 </div>
-            </div>
-            <div class="card-sala">
-                <div class="card-content">
-                    <div class="card-title">B201</div>
-                    <div class="card-body">
-                        <div class="card-element">Capacidad: 00</div>
-                        <div class="card-element">Equipos de Computo: 00</div>
-                        <div class="card-element">Aire Acondicionado: Si</div>
-                        <div class="card-element">Conexi&oacute;n TV: HDMI: 00 VGA: 00</div>
-                        <div class="card-element">Tablero: Acr&iacute;lico, Tiza</div>
-                    </div>
-                    <div class="card-footer">
-                        <div class="card-element enlace"><a href="#">VER HORARIOS</a></div>
-                    </div>
-                </div>
-            </div>
-            <div class="card-sala">
-                <div class="card-content">
-                    <div class="card-title">B201</div>
-                    <div class="card-body">
-                        <div class="card-element">Capacidad: 00</div>
-                        <div class="card-element">Equipos de Computo: 00</div>
-                        <div class="card-element">Aire Acondicionado: Si</div>
-                        <div class="card-element">Conexi&oacute;n TV: HDMI: 00 VGA: 00</div>
-                        <div class="card-element">Tablero: Acr&iacute;lico, Tiza</div>
-                    </div>
-                    <div class="card-footer">
-                        <div class="card-element enlace"><a href="#">VER HORARIOS</a></div>
-                    </div>
-                </div>
-            </div>
-            <div class="card-sala">
-                <div class="card-content">
-                    <div class="card-title">B201</div>
-                    <div class="card-body">
-                        <div class="card-element">Capacidad: 00</div>
-                        <div class="card-element">Equipos de Computo: 00</div>
-                        <div class="card-element">Aire Acondicionado: Si</div>
-                        <div class="card-element">Conexi&oacute;n TV: HDMI: 00 VGA: 00</div>
-                        <div class="card-element">Tablero: Acr&iacute;lico, Tiza</div>
-                    </div>
-                    <div class="card-footer">
-                        <div class="card-element enlace"><a href="#">VER HORARIOS</a></div>
-                    </div>
-                </div>
-            </div>
-            <div class="card-sala">
-                <div class="card-content">
-                    <div class="card-title">B201</div>
-                    <div class="card-body">
-                        <div class="card-element">Capacidad: 00</div>
-                        <div class="card-element">Equipos de Computo: 00</div>
-                        <div class="card-element">Aire Acondicionado: Si</div>
-                        <div class="card-element">Conexi&oacute;n TV: HDMI: 00 VGA: 00</div>
-                        <div class="card-element">Tablero: Acr&iacute;lico, Tiza</div>
-                    </div>
-                    <div class="card-footer">
-                        <div class="card-element enlace"><a href="#">VER HORARIOS</a></div>
-                    </div>
-                </div>
-            </div>
+            </div>-->
+            
+            
+           
         </div>
         <footer>
             <?php
@@ -202,6 +208,7 @@
 
     <script src="js/slide.js"></script>
     <script src="js/soliOut.js"></script>
+    <script src="js/cards.js"></script>
 </body>
 
 </html>
