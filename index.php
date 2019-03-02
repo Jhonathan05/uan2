a5cebaadb6df22bbbbf696a966b1a64dfe29bbc6
Jhonathan <jacireno@hotmail.com>
2019-03-01 19:21:38 -0500

    Correccion en UI index móvil

--- index.php
+++ index.php
@@ -16,19 +16,19 @@
             <div class="content-index-header">
                 <img src="img/logoBlanco.png" alt="Uan Logo">
             </div>
-            <div class="content-index-body"></div>
+
             <div class="index-login">
                 <form action="">
-                   <div class="login-element">
-                       <input type="text" class="i-base" placeholder="Usuario" required>
-                       <span><a href="#">Forgot?</a></span>
-                   </div>
-                   <div class="login-element">
-                       <input type="password" class="i-base" placeholder="Contraseña" required>
-                       <span><a href="#">Forgot?</a></span>
-                   </div>
-                   <div class="problem"> <a href="#">Problemas al iniciar sesi&oacute;n?</a></div>
-                   <button type="submit">Iniciar Sesi&oacute;n</button>
+                    <div class="login-element">
+                        <input type="text" class="i-base" placeholder="Usuario" required>
+                        <span><a href="#">Forgot?</a></span>
+                    </div>
+                    <div class="login-element">
+                        <input type="password" class="i-base" placeholder="Contraseña" required>
+                        <span><a href="#">Forgot?</a></span>
+                    </div>
+                    <div class="problem"> <a href="#">Problemas al iniciar sesi&oacute;n?</a></div>
+                    <button type="submit">Iniciar Sesi&oacute;n</button>
                 </form>
             </div>
             <div class="divisor"></div>
@@ -39,21 +39,24 @@
                         <button type="submit" onclick="alerta()">Estado de Solicitud</button>
                     </form>
                 </div>
-              <!--  <div class="index-links">
+                <!--  <div class="index-links">
                     <a href="">Bloque B</a>
                     <a href="">Bloque C</a>
                 </div>-->
             </div>
 
+
         </div>
         <div id="arrow-down" class="arrow active"><a href="#footer-index"><i class="fas fa-angle-down"></i></a></div>
-        <div id="arrow-up" class="arrow-up"><a href="#container-index"><i class="fas fa-angle-up"></i></a></div>
+        
     </div>
+    
     <footer id="footer-index">
-            <?php
+       <div id="arrow-up" class="arrow-up"><a href="#container-index"><i class="fas fa-angle-up"></i></a></div>
+        <?php
                 include_once('./mod/footer.php');
             ?>
-        </footer>
+    </footer>
     <script>
         function alerta() {
             alert("No existe solicitud alguna.");
